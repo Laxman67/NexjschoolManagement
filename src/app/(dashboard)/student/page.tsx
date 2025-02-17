550d0490fd86dd65e75a831ecd2516db2c83cc40
Laxman67 <laxmanhenricks94@gmail.com>
2025-02-17 22:30:16 +0530

    CalenderEvent Classes Student

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,27 @@
+import Announcements from '@/components/Announcements';
+import BigCalender from '@/components/BigCalender';
+import EventCalender from '@/components/EventCalender';
 import React from 'react';
 
 const StudentPage = () => {
-  return <div>Student</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full p-4 bg-white rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalender />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full lg:w-1/3">
+        <div>
+          <EventCalender />
+          <Announcements />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
