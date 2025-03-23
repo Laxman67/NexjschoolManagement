d4321944068e9f9327e5e055e6fc1c2a1c29d2e9
Laxman67 <laxmanhenricks94@gmail.com>
2025-03-23 17:23:51 +0530

    All Template Defined in UI

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,17 +1,15 @@
 import Paginition from '@/components/Paginition';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role, eventsData } from '@/lib/data';
+import { role, announcementsData } from '@/lib/data';
 import Image from 'next/image';
 import Link from 'next/link';
 
-type Assignment = {
+type Announcement = {
   id: number;
   title: string;
   class: string;
   date: number;
-  startTime: string;
-  endTime: string;
 };
 
 const columns = [
@@ -26,24 +24,15 @@ const columns = [
     accessor: 'date',
     className: 'hidden md:table-cell',
   },
-  {
-    header: 'Start Time',
-    accessor: 'startDate',
-    className: 'hidden md:table-cell',
-  },
-  {
-    header: 'End Time',
-    accessor: 'endDate',
-    className: 'hidden md:table-cell',
-  },
+
   {
     header: 'Actions',
     accessor: 'actions',
   },
 ];
 
-const EventsList = () => {
-  const renderRow = (item: Assignment) => (
+const AnnouncementsList = () => {
+  const renderRow = (item: Announcement) => (
     <tr
       key={item.id}
       className=" border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight 
@@ -52,12 +41,11 @@ const EventsList = () => {
       <td className="flex items-center gap-4 p-4">{item.title}</td>
       <td className="hidden  md:table-cell">{item.class}</td>
       <td className="hidden  md:table-cell">{item.date}</td>
-      <td className="hidden  md:table-cell">{item.startTime}</td>
-      <td className="hidden  md:table-cell">{item.endTime}</td>
+
       <td>
         <div className="flex items-center gap-2">
           <Link
-            href={`/list/events/${item.id}`}
+            href={`/list/announcements/${item.id}`}
             className="flex items-center justify-between gap-2"
           >
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
@@ -84,7 +72,9 @@ const EventsList = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 h-[100%]">
       {/* Top */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Announcements
+        </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -102,11 +92,11 @@ const EventsList = () => {
       </div>
 
       {/* List */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData} />
+      <Table columns={columns} renderRow={renderRow} data={announcementsData} />
       {/* Paginition */}
       <Paginition />
     </div>
   );
 };
 
-export default EventsList;
+export default AnnouncementsList;
